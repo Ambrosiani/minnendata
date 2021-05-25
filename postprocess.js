85516d0051a11f40b8612d4d9d23545902ea36c5
Aron Ambrosiani <aron@ambrosiani.se>
2021-05-25 21:23:23 +0200

    preprocess html

--- postprocess.js
+++ postprocess.js
@@ -25,7 +25,25 @@ var sortedArray = array.sort( GetSortOrder("id"));
 
 var responsesWithImages = 0;
 var responsesWithCoordinates = 0;
-var stats = {}
+var stats = {};
+var indexhtml = '<!DOCTYPE html>\
+<html lang="sv">\
+<head>\
+    <meta charset="utf-8">\
+    <meta name="viewport" content="width=device-width, initial-scale=1">\
+    <title>Återföreningen – livet efter vaccinet</title>\
+    <style type="text/css">\
+      img {\
+          display: block;\
+          height: auto;\
+          max-width: 100%;\
+      }\
+    </style>\
+<link rel="stylesheet" href="https://unpkg.com/flexmasonry/dist/flexmasonry.css">\
+<script src="https://unpkg.com/flexmasonry/dist/flexmasonry.js"></script>\
+</head>\
+<body>\
+<div class="grid">';
 
 sortedArray.forEach(function(response){
   if (response.hasOwnProperty('dms_url')) {
@@ -33,6 +51,9 @@ sortedArray.forEach(function(response){
     let newURL = 'https://dms01.dimu.org'+url.pathname;
     response['dms_url'] = newURL;
     responsesWithImages++;
+    var imgstring = '<div><img src="'+response.dms_url+'></div>\
+    ';
+    indexhtml += imgstring;
   }
   if (response.latitude !== null) {
     responsesWithCoordinates++;
@@ -44,10 +65,18 @@ stats["responsesWithCoordinates"] = responsesWithCoordinates;
 stats["responsesWithImages"] = responsesWithImages;
 
 // Step 3. Write a new JSON file with our filtered data
-const newFilename = `eftervaccinet-postprocessed.json` // name of a new file to be saved
+const newFilename = 'eftervaccinet-postprocessed.json';
 
-await Deno.writeTextFile(newFilename, JSON.stringify(sortedArray, null, 2))
-await Deno.writeTextFile('stats.json', JSON.stringify(stats, null, 2))
-console.log("Wrote a post process file")
+indexhtml += '</div>\
+\
+<script>\
+    FlexMasonry.init(".grid");\
+</script>\
+</html>';
 
-removeFile(filename)
+await Deno.writeTextFile(newFilename, JSON.stringify(sortedArray, null, 2));
+await Deno.writeTextFile('stats.json', JSON.stringify(stats, null, 2));
+await Deno.writeTextFile('index.html', indexhtml);
+console.log("Wrote a post process file");
+
+removeFile(filename);
