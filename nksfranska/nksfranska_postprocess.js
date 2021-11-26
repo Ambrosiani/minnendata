b57a08d983d511bb97f0fd7cd41fccdc5232410c
Aron Ambrosiani <aron@ambrosiani.se>
2021-11-26 10:23:36 +0100

    Update nksfranska_postprocess.js

--- nksfranska/nksfranska_postprocess.js
+++ nksfranska/nksfranska_postprocess.js
@@ -2,7 +2,7 @@
 
 // Helper library written for useful postprocessing tasks with Flat Data
 // Has helper functions for manipulating csv, json, excel, zip, and image files
-import { readJSON } from 'https://deno.land/x/flat@0.0.10/src/json.ts'
+import { readJSON, readJSONFromURL } from 'https://deno.land/x/flat@0.0.10/src/json.ts'
 import { removeFile } from 'https://deno.land/x/flat@0.0.10/src/remove.ts'
 
 function GetSortOrder(prop){
